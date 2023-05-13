556f459838488244c2f3fa44929cf0c685c3e68c
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-05-13 16:33:15 +1000

    test: add transform test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,8 @@
-from aircraft.etl_flow import extract_live_data, extract_reference_data
+from aircraft.etl_flow import extract_live_data, extract_reference_data, transform
+import pandas as pd
+import numpy as np
+
+from aircraft.etl_flow import cleaned_fields
 
 
 def test_extract_live_data():
@@ -26,3 +30,56 @@ def test_extract_reference_data():
         "latitude": "-17.3542",
         "longitude": "-145.4961",
     }
+
+
+def test_transform() -> None:
+    raw_aircraft_data = pd.DataFrame(
+        {
+            "icao24": "XYZ",
+            "callsign": "AA",
+            "origin_country": "United States",
+            "time_position": 1,
+            "last_contact": 2,
+            "longitude": -12.34,
+            "latitude": 45.67,
+            "baro_altitude": 123.45,
+            "on_ground": False,
+            "velocity": 1.29,
+            "true_track": 39.38,
+            "vertical_rate": np.nan,
+            "sensors": None,
+            "geo_altitude": np.nan,
+            "squawk": None,
+            "spi": False,
+            "position_source": 0,
+        },
+        index=[0],
+    )
+
+    airlines = {"AA": "American Airlines", "DL": "Delta Air Lines"}
+
+    expected = pd.DataFrame(
+        [
+            {
+                "icao": "XYZ",
+                "callsign": "AA",
+                "time_position": 1,
+                "last_contact": 2,
+                "longitude": -12.34,
+                "latitude": 45.67,
+                "baro_altitude": 123.45,
+                "on_ground": False,
+                "velocity": 1.29,
+                "true_track": 39.38,
+                "vertical_rate": np.nan,
+                "geo_altitude": np.nan,
+                "airline": "AA",
+            }
+        ],
+        columns=cleaned_fields,
+    )
+
+    # flyte only supports keyword args when calling tasks
+    actual = transform(raw_aircraft_data=raw_aircraft_data, airlines=airlines)
+
+    pd.testing.assert_frame_equal(actual.open().all(), expected)
